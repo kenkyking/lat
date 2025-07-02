a9df6e38498904b32a5d798765706a54fe429671
Nadhif <nadhifbsl@gmail.com>
2025-07-02 22:44:21 +0700

    menambahkan data transaksi pembelian

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,9 +20,25 @@ $routes->group('produk', ['filter' => 'auth'], function ($routes) {
     $routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
     $routes->post('produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
     $routes->get('produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
+    $routes->get('download','ProdukController::download');
 });
 
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+});
+
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+
+$routes->get('get-location', 'TransaksiController::getLocation', ['filter' => 'auth']);
+$routes->get('get-cost', 'TransaksiController::getCost', ['filter' => 'auth']);
+
 
 $routes->get('FAQ', 'FaqController::index', ['filter' => 'auth']);
 $routes->get('profile', 'Home::profile', ['filter' => 'auth']);
